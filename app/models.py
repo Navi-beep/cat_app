eb198864eb084027a12aebe20f97807f7d997e0f
Navi-Beep <estawars30@gmail.com>
2022-09-10 20:19:45 -0500

    added cat db

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(70), nullable=False, unique=True)
     password = db.Column(db.String(250), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    
+
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -34,3 +34,20 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
+class Add_Cat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fav_cat_breed = db.Column(db.String(50), nullable=False)
+    fav_int_cat = db.Column(db.String(75), nullable=False)
+    fav_cat_fact = db.Column(db.String(300), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        db.session.add(self)
+        db.session.commit()
+
+
+    def __repr__(self):
+        return f'<Add_Cat {self.id} | {self.fav_int_cat}>'
